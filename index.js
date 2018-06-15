00c75f3f640ee6a337a78c2c7e08eccfdd9ea069
Lauren Days <lmchambers0420@gmail.com>
2018-06-15 01:26:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,14 @@ function happyHolidays() {
   return "Happy holidays!";
 }
 
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`;
+}
 
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`;
+}
 
+function holidayCountdown(holiday, number) {
+  return `It\'s ${number} days until ${holiday}!`;
+}
